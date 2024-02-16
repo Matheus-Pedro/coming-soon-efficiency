8d1059ae4423fdea09a822f1ad33265103f600fd
Matheus-Pedro <matheus.caprioli.pedro@gmail.com>
2024-02-15 21:29:56 -0300

    Create StyleSheet for Email Form Component

--- src/components/emailForm.js
+++ src/components/emailForm.js
@@ -1,10 +1,10 @@
-import '../styles/emailForm.css'
+import StyleSheet from '../styles/emailForm.css'
 
 export default function EmailForm () {
     return(
         // onsubmit também...
-        <form method='post' action='data'>
-            <input calssName="email-entry" type="email" placeholder="Endereço de e-mail..."></input>
+        <form method='post' action='data' style={StyleSheet}>
+            <input className="email-entry" type="email" placeholder="Endereço de e-mail..."></input>
             <input className = "remember-me" type='submit' value='Lembre-me'/>
         </form>
     );
